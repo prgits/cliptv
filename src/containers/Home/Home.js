dd0fdea5760f478b2d46df740070d9cdfd33caad
Thinh Nguyen Viet <thinhnv@vega.com.vn>
2017-06-05 16:08:23 +0700

    fd

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,12 +3,24 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 class Home extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      focus: 1
+    };
+  }
+
   render() {
     return (
       <div className="welcome">
         <div className="welcome-content">
-          <Link to="/register-trial" className="btn btn-gray">Dùng thử 1 tháng miễn phí</Link>
-          <Link to="/login" className="btn btn-gray btn-login ml10">Đăng nhập</Link>
+          <Link to="/register-trial" className={(this.state.focus === 1) ? 'btn btn-gray focus' : 'btn btn-gray'}>
+            Dùng thử 1 tháng miễn phí
+          </Link>
+          <Link to="/login" className={this.state.focus === 2 ? 'btn btn-gray btn-login ml10 focus':
+          'btn btn-gray btn-login ml10'}>
+            Đăng nhập
+          </Link>
         </div>
       </div>
     );
